cf081e9f2e3caf00548d5fa1937d76651ac4c271
Sam Bonnell <sbonnell147@gmail.com>
2024-04-28 12:53:25 -0700

    This is a commit

--- tests/position_test.py
+++ tests/position_test.py
@@ -1,6 +1,8 @@
+# Default libraries
 import time
 import os
 
+# Local Classes
 import drone
 
 import cflib.crtp
@@ -18,34 +20,18 @@ if __name__ == '__main__':
     drone_thread = drone.drone_thread_class()
     drone_thread.name = "drone_thread"
     drone_thread.daemon = True
+    drone_thread.start()
 
-    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        print("Synced with drone...")
-        with PositionHlCommander(scf, default_height=0.5, controller=PositionHlCommander.CONTROLLER_PID) as pc:
-            print("Configured Position Commander...")
-            while True:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print("Please select an index to set: ")
-                try:
-                    index = int(input())
-                    if index < 1 or index > 9:
-                        raise ValueError #this will send it to the print message and back to the input option
-                    break
-                except:
-                    print("Not a valid option!")
-                    time.sleep(1)
-
-            drone_thread.start()
-            while index is not 0:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print("Please select an index to set: ")
-                try:
-                    index = int(input())
-                    if index < 0 or index > 9:
-                        raise ValueError #this will send it to the print message and back to the input option
-                    break
-                except:
-                    print("Not a valid option!")
-                    time.sleep(1)
-                
-                drone_thread.set_position(index)
+    while index is not 0:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Please select an index to set: ")
+        try:
+            index = int(input())
+            if index < 0 or index > 9:
+                raise ValueError #this will send it to the print message and back to the input option
+            break
+        except:
+            print("Not a valid option!")
+            time.sleep(1)
+        
+        drone_thread.set_position(index)
