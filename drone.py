092ad2df74d21796faa572efe44bb96f4ec9d7a8
Sam Bonnell <sbonnell147@gmail.com>
2024-04-28 11:07:55 -0700

    this is a commit

--- drone.py
+++ drone.py
@@ -2,6 +2,7 @@
 import os
 import time
 import threading
+import csv
 
 # cfclient libraries
 import cflib.crtp
@@ -12,11 +13,15 @@ from cflib.crazyflie.syncLogger import SyncLogger
 from cflib.positioning.position_hl_commander import PositionHlCommander
 from cflib.utils import uri_helper
 
+Z_HEIGHT = 0.15
+
 class drone_thread_class(threading.Thread):
     def __init__(self):
         super(drone_thread_class, self).__init__()
-        # Initialize the low-level drivers (don't list the debug drivers)
+        self.data = [None]
+
         try:
+            # Initialize the low-level drivers (don't list the debug drivers)
             cflib.crtp.init_drivers(enable_debug_driver=False)
             self.uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
             self.scf = SyncCrazyflie(self.uri, cf=Crazyflie(rw_cache='./cache'))
@@ -36,7 +41,7 @@ class drone_thread_class(threading.Thread):
             
     def init_drone_board(self):
         default_index_set = {1,2,3,4,5,6,7,8,9}
-        drone_board = [None] * 9
+        self.drone_board = [None] * 9
 
         while len(default_index_set):
             while True:
@@ -57,18 +62,19 @@ class drone_thread_class(threading.Thread):
             # Add the current position of the drone into the index of the 
             default_index_set.remove(index)
             [self.x_position, self.y_position, self.z_position] = self.pc.get_position()
-            self.z_position = 0.15
-            drone_board[index - 1] = (self.x_position, self.y_position)
+            self.z_position = Z_HEIGHT
+            self.drone_board[index - 1] = (self.x_position, self.y_position)
 
-    def estimate_drone_position(self) -> None:
+        self.x_position = 0
+        self.y_position = 0
+        self.z_position = 0
+
+    def log_position(self) -> None:
         cf = self.scf.cf
         cf.param.set_value('kalman.resetEstimation', '1')
         time.sleep(0.1)
         cf.param.set_value('kalman.resetEstimation', '0')
 
-        self.wait_for_estimate()
-
-    def wait_for_estimate(self) -> None:
         log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
         log_config.add_variable('kalman.varPX', 'float')
         log_config.add_variable('kalman.varPY', 'float')
@@ -76,17 +82,25 @@ class drone_thread_class(threading.Thread):
 
         with SyncLogger(self,log_config) as logger:
             for log_entry in logger:
-                data = log_entry[1]
+                self.data.append(log_entry[1])
 
-                self.x_position = data['kalman.varPX']
-                self.y_position = data['kalman.varPY']
-                self.z_position = data['kalman.varPZ']
+    def export_position_log(self, file_name) -> bool:
+        with open(file_name, 'w', newline='') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for row in self.data:
+                filewriter.writerow(row)
+
+    def export_drone_board(self, file_name) -> bool:
+        with open(file_name, 'w', newline='') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for index in range(10):
+                filewriter.writerow(self.drone_board[index])
 
     # Function to move the drone to an index 
     def set_position(self, index) -> None:
         self.x_position = self.drone_board[index].first
         self.y_position = self.drone_board[index].second
-        self.z_position = 0.15
+        self.z_position = Z_HEIGHT
 
     def return_reached_position(self, index):
         threshold = 0.05
