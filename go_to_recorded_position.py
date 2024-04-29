71797f0079c8bf9f805ba41c75bbd1722293a6b8
GraemeDockrill <graeme.dockrill01@gmail.com>
2024-04-28 17:25:42 -0700

    Got drone positions saved and started flying, need to tune the positioning

--- go_to_recorded_position.py
+++ go_to_recorded_position.py
@@ -13,27 +13,31 @@ from cflib.utils import uri_helper
 
 import threading
 
-# URI to the Crazyflie to connect to
-uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
 
-MAX_VELOCITY = 0.25
-SLEEP_TIME = 3
-
-cflib.crtp.init_drivers()
 
  
 class DroneMovement(threading.Thread):
     def __init__(self):
         super(DroneMovement, self).__init__()
+
+        cflib.crtp.init_drivers()
+
+        # URI to the Crazyflie to connect to
+        self.uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+        self.MAX_VELOCITY = 0.25
+        self.SLEEP_TIME = 3
         
         # array of saved positions for each board tile
-        self.recorded_positions_xy = [None]
+        self.recorded_positions_xy = [(None, None)] * 9
 
         # current drone position from lighthouse
-        self.x_position
-        self.y_position
-        self.z_position
-        self.set_velocity = 0.25
+        self.x_current_position = 0
+        self.y_current_position = 0
+
+        # Velocity and Position Constants
+        self.z_target_position = 0.15
+        self.set_velocity = 0.10
 
         # saved target position index
         self.target_pos_index = 0
@@ -47,35 +51,71 @@ class DroneMovement(threading.Thread):
 
         # infinite loop for control
         while True:
-
+            print("On-standby")
             # check if drone released for movement
             if self.if_run:
+                print("Running")
 
                 # connect to drone
-                with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+                with SyncCrazyflie(self.uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+
+                    print("Connected to drone")
                     # start position commander
-                    with PositionHlCommander(self.scf, controller=PositionHlCommander.CONTROLLER_PID) as pc:
+                    with PositionHlCommander(scf) as pc:
+                        # time.sleep(1)
 
-                        # control loop for drone movement
-                        while self.if_run:
-                            # make drone move (x, y, z, velocity)
-                            pc.go_to(self.x_position, self.y_position, self.z_position, self.set_velocity)
-                            # delay to let drone move
-                            time.sleep(0.5)
+                        print("HL commander started")
 
-                        # when drone no longer running, land the drone
+                        # initialize log configuration
+                        log_config = LogConfig(name='State Estimate', period_in_ms=250)
+                        log_config.add_variable('stateEstimate.x', 'float')
+                        log_config.add_variable('stateEstimate.y', 'float')
+                        log_config.add_variable('stateEstimate.z', 'float')
 
+
+                        # start reading drone memory for lighthouse position
+                        with SyncLogger(scf, log_config) as logger:
+
+                            print("Started SyncLogger")
+
+                            
+                            # control loop for drone movement
+                            while self.if_run:
+
+                                # get current drone position for comparison
+                                # reading through drone data
+                                for log_entry in logger:
+                                    data = log_entry[1]
+
+                                    # print('[%d][%s]: %s' % (timestamp, logconf_name, data))
+
+                                    # add recorded x,y position to array
+                                    self.x_current_position = data['stateEstimate.x']
+                                    self.y_current_position = data['stateEstimate.y']
+
+                                    print(self.x_current_position + ", " + self.y_current_position)
+                                    print(self.self.recorded_positions_xy[self.target_pos_index].first + ", " + self.recorded_positions_xy[self.target_pos_index].second)
+
+                                #logger.next()
+
+                                # make drone move (x, y, z, velocity)
+                                pc.go_to(self.recorded_positions_xy[self.target_pos_index].first, self.recorded_positions_xy[self.target_pos_index].second, self.z_target_position, self.set_velocity)
+                                # delay to let drone move
+                                time.sleep(1)
+
+                            # when drone no longer running, land the drone
+                            pc.land()
 
                 break
 
             # small time delay
-            time.sleep(0.5)
+            time.sleep(2)
 
 
     # function to check if drone reached setpoint
     def target_reached(self) -> bool:
         # check if drone within tolerance to setpoint
-        if (abs(self.x_position - self.drone_board[self.target_pos_index].first) < self.target_pos_threshold) and (abs(self.y_position - self.drone_board[self.target_pos_index].second) < self.target_pos_threshold):
+        if (abs(self.x_current_position - self.drone_board[self.target_pos_index].first) < self.target_pos_threshold) and (abs(self.y_current_position - self.drone_board[self.target_pos_index].second) < self.target_pos_threshold):
             return True
         else:
             return False
@@ -84,7 +124,7 @@ class DroneMovement(threading.Thread):
     def record_tile_position(self, index) -> None:
 
         # connect to drone
-        with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+        with SyncCrazyflie(self.uri, cf=Crazyflie(rw_cache='./cache')) as scf:
             # initialize log configuration
             log_config = LogConfig(name='State Estimate', period_in_ms=250)
             log_config.add_variable('stateEstimate.x', 'float')
@@ -102,7 +142,7 @@ class DroneMovement(threading.Thread):
                     print('[%d][%s]: %s' % (timestamp, logconf_name, data))
 
                     # add recorded x,y position to array
-                    self.recorded_positions_xy[index] = (data['stateEstimate.x'], data['stateEstimate.y'])
+                    self.recorded_positions_xy[index-1] = (data['stateEstimate.x'], data['stateEstimate.y'])
 
                     break
 
@@ -110,6 +150,10 @@ class DroneMovement(threading.Thread):
     def set_drone_tile_target(self, index) -> None:
         # update target index for use in run() function
         self.target_pos_index = index
+
+    # debugging function to print recorded drone positions
+    def print_recorded_positions_xy(self) -> None:
+        print(self.recorded_positions_xy)
 
     # function to start drone movement connection
     def start_drone(self) -> None:
@@ -123,8 +167,41 @@ class DroneMovement(threading.Thread):
 # main program
 if __name__ == '__main__':
 
+    default_index_set = {1,2,3,4,5,6,7,8,9}
+
     # create drone thread
     drone_thread = DroneMovement()
     drone_thread.name = "thread_thread_name"
     drone_thread.daemon = True
     drone_thread.start()
+
+    # loop for all tiles
+    while len(default_index_set) > 0:
+        # continuously prompt user for input
+        while True:
+            print("Please select an index to set: ")
+            print(default_index_set)
+            try:
+                index = int(input())
+                if index < 1 or index > 9:
+                    raise ValueError #this will send it to the print message and back to the input option
+                if index not in default_index_set:
+                    raise ValueError
+                break
+            except:
+                print("Not a valid option!")
+                time.sleep(0.25)
+            print("")
+
+        default_index_set.remove(index)
+        # record current drone position
+        drone_thread.record_tile_position(index)
+
+    # print the calibration positions
+    drone_thread.print_recorded_positions_xy()
+    
+    # start drone
+    drone_thread.start_drone()
+    drone_thread.set_drone_tile_target(0)
+    time.sleep(3)
+    drone_thread.stop_drone()
